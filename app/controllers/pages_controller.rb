63846e4580ec59a36907890221dae0c383bb1a40
Marcelo E. Cardoso <marc.cardo@gmail.com>
2014-11-27 03:09:53 -0200

    Adds header partial. Provides redirection to home or new profile accordingly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      @profile = current_user.profile
+      if !@profile.nil? # if user has a profile, send him there
+        redirect_to profile_path(@profile.id)
+      else
+        redirect_to new_profile_path # if not, go create one
+      end
+    # else : load sispep starting page      
+    end
   end
 
   def help
